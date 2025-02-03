97e36b669b3540bffa94802aa7a34a346eabb14c
Zainab Aamir <zaamir427@gmail.com>
2025-02-03 20:24:13 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = requests.get("https://news.ycombinator.com/news").text
+soup = BeautifulSoup(url, 'lxml')
+
+# print(requests.get("https://news.ycombinator.com/news").status_code)
+mainContent = soup.find('table')
+# print(mainContent)
+
+for news in mainContent.find_all('td', class_ = 'title'):
+    
+    print(news.text)
+
+for points in mainContent.find_all('span', class_ = 'score'):
+    print(points.text)
+
+    print(' ')
+    print('')
+
+
+
