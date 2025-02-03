01689dcb344c5322a07213eebf258406f31600c1
Zainab Aamir <zaamir427@gmail.com>
2025-02-03 20:52:24 +0500

    initial commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ soup = BeautifulSoup(url, 'lxml')
 
 # print(requests.get("https://news.ycombinator.com/news").status_code)
 mainContent = soup.find('body')
+print(mainContent)
 # print(mainContent)
 
 # for news in mainContent.find_all('td', class_ = 'title'):
